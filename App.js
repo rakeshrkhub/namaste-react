ecdd375c210ccb1be060ed143b42684f878acbd4
rakeshrkhub <rakeshkumarrambad@gmail.com>
2023-11-11 18:49:39 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+const heading=React.createElement(
+    "div",
+    {id:"parent"},
+    [
+        React.createElement(
+            "div",
+            {id:"child1",class:"child1"},
+            [
+                React.createElement(
+                    "h2",
+                    {id:"upper"},
+                    "I'm h1 tag!"
+                ),
+                React.createElement(
+                    "h2",
+                    {id:"lower"},
+                    "I'm h2 tag!!"
+                )
+            ]),
+            React.createElement(
+                "div",
+                {id:"child2",class:"child2"},
+                [
+                    React.createElement(
+                        "h3",
+                        {id:"upper"},
+                        "I'm h3 tag!"
+                    ),
+                    React.createElement(
+                        "h4",
+                        {id:"lower"},
+                        "I'm h4 tag!!"
+                    )
+                ])
+    ]
+    );
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
